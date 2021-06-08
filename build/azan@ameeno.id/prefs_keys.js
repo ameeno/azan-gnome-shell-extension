60d39b17bf55a2e6790dac83981d5b84a2bf6674
Ameeno <ameeno@users.noreply.github.com>
2021-06-09 00:50:33 +0100

    Added: build/azan@ameeno.id/HijriCalendarKuwaiti.js, build/azan@ameeno.id/PrayTimes.js, build/azan@ameeno.id/convenience.js, build/azan@ameeno.id/extension.js, build/azan@ameeno.id/metadata.json, build/azan@ameeno.id/org.gnome.shell.extensions.azan.gschema.xml, build/azan@ameeno.id/prefs.js, build/azan@ameeno.id/prefs_keys.js, build/azan@ameeno.id/schemas/gschemas.compiled, build/azan@ameeno.id/schemas/org.gnome.shell.extensions.azan.gschema.xml, build/azan@ameeno.id/stylesheet.css Modified: .gitignore, extension.zip

---
+++ build/azan@ameeno.id/prefs_keys.js
@@ -0,0 +1,7 @@
+var AUTO_LOCATION = 'auto-location';
+var CALCULATION_METHOD = 'calculation-method';
+var LATITUDE = 'latitude';
+var LONGITUDE = 'longitude';
+var TIMEZONE = 'timezone';
+var TIME_FORMAT_12 = 'time-format-12';
+var CONCISE_LIST = 'concise-list';
